45abf999211c9b93fce43463e00acce08345e9dd
mpollak <marcellopollak123@gmail.com>
2025-07-10 14:49:18 -0400

    added proper salary truncation to two places after decimal

--- ml_modeling/query_model.py
+++ ml_modeling/query_model.py
@@ -2,6 +2,7 @@ import pickle as pkl
 import sys
 import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
+import math
 
 PICKLE_MODEL_DUMP = './model_dump.pkl'
 PICKLE_ENCODER_DUMP = './encoder_dump.pkl'
@@ -21,5 +22,7 @@ with open(PICKLE_ENCODER_DUMP, 'rb') as f:
 features_df = pd.DataFrame({'role' : [role], 'work_location' : [work_location] })
 X_encoded = encoder.transform(features_df)
 X_encoded = pd.DataFrame(X_encoded)
-predicted_salary = model.predict(X_encoded)
-print(predicted_salary)
+predicted_salary = model.predict(X_encoded)[0]
+
+
+print(f"{predicted_salary:.2f}")
