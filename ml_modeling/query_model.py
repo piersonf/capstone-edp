5595c77c97862e7f1459de43034fa50f6e23ce98
mpollak <marcellopollak123@gmail.com>
2025-07-10 14:38:32 -0400

    implemented proper model querying in query_model.py

--- ml_modeling/query_model.py
+++ ml_modeling/query_model.py
@@ -1,14 +1,25 @@
 import pickle as pkl
 import sys
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder
+
+PICKLE_MODEL_DUMP = './model_dump.pkl'
+PICKLE_ENCODER_DUMP = './encoder_dump.pkl'
+
+# _, work_location, role = sys.argv
+
+work_location = 'Hartford'
+role = 'Product Manager'
+
+with open(PICKLE_MODEL_DUMP, 'rb') as f:
+    model = pkl.load(f)
+
+with open(PICKLE_ENCODER_DUMP, 'rb') as f:
+    encoder = pkl.load(f)
 
 
-
-PICKLE_MODEL_DUMP = 'model_dump.pkl'
-
-work_location, role = sys.argv[1:2+1]
-
-# with open(PICKLE_MODEL_DUMP, 'rb') as f:
-#     model = pkl.load(f)
-
-raise Exception('Testing PythonShell failure')
-print(f'Work Location: {work_location}, Role: {role}')
+features_df = pd.DataFrame({'role' : [role], 'work_location' : [work_location] })
+X_encoded = encoder.transform(features_df)
+X_encoded = pd.DataFrame(X_encoded)
+predicted_salary = model.predict(X_encoded)
+print(predicted_salary)
